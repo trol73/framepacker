29781626568adffcf3abac455b91cd6f51936b96
Oleg Trifonov <otrifonow@gmail.com>
2013-11-20 21:34:16 +0400

    added test data

---
+++ test.sh
@@ -0,0 +1,3 @@
+java -jar dist/framepacker.jar test/lights.png 4x3 test/lights_out.png -t=15
+java -jar dist/framepacker.jar test/particles.png 6x6 test/particles_out.png -t=15
+java -jar dist/framepacker.jar test/stars.png 8x5 test/stars_out.png -t=5
